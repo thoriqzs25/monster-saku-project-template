3fdfb98c91a3572813d80049d8f06b009c01006a
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2022-03-31 17:29:34 +0700

    Add files via upload

---
+++ src/main/java/com/monstersaku/Player.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Player{
+    private String name;
+    private List<Monster> listOfMonster = new ArrayList<Monster>();
+    private int numOfMonster;
+    public Player(String name, List<Monster> listOfMonster){
+        this.name = name;
+        this.listOfMonster = listOfMonster;
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void addMonster(Monster monster){
+        listOfMonster.add(monster);
+    }
+    public void countMonster(List<Monster> listOfMonster){
+        for(Monster monster:listOfMonster){
+            if (monster.getBS().getHealthPoint() != 0) {
+                numOfMonster += 1;
+            } else {
+                numOfMonster += 0;
+            }
+           
+        }
+    }
+    public int outputCount(){
+        return this.numOfMonster;
+    }
+}
